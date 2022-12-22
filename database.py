cadef86c47827170b2ef05c1de02b5d943c77f63
verapasichenko <115879263+verapasichenko@users.noreply.github.com>
2022-12-22 13:08:09 +0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+class Database:
+    """Обертка для работы с БД"""
+
+    def __init__(self):
+        """Инициализация базы данных"""
+        self.conn = sqlite3.connect('db/project.db', check_same_thread=False)
+        self.cursor = self.conn.cursor()
+        self.check_if_table_exists()
+    
+    def check_if_table_exists(self):
+        ''' Проверяем, есть ли таблица reviews в базе данных. Если нет, то создаем ее. '''
+        self.cursor.execute('SELECT name FROM sqlite_master WHERE type="table" AND name="reviews"')
+        if self.cursor.fetchone() is None:
+            self.cursor.execute('CREATE TABLE reviews (id_review INTEGER, review TEXT)')
+            self.conn.commit()
+
+    def add_review(self, message):
+        ''' Добавляем отзыв в базу данных '''
+        self.cursor.execute('INSERT INTO reviews (id_review, review) VALUES (?, ?)',
+                   (message.from_user.id, message.text))
+        self.conn.commit()
+
+    def get_reviews(self):
+        ''' Получаем все отзывы из базы данных'''
+        self.cursor.execute('SELECT review FROM reviews')
+        return self.cursor.fetchall()
