4c7dad3087af4df69e12a9eb5d62c9a1cfa8db23
verapasichenko <115879263+verapasichenko@users.noreply.github.com>
2022-12-22 13:08:43 +0300

    Add files via upload

---
+++ stations.py
@@ -0,0 +1,157 @@
+stations_data = {
+"комсомольская" : ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d4575760-Komsomol_Station-Moscow_Central_Russia.html",
+                    "https://www.tripadvisor.ru/AttractionsNear-g298484-d15266271-Metro_Station_Komsomolskaya-Moscow_Central_Russia.html",
+                    "https://www.tripadvisor.ru/AttractionsNear-g298484-d4575760-Komsomol_Station-Moscow_Central_Russia.html"),
+
+"курская" : ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d9575877-Kurskaya-Moscow_Central_Russia.html",
+            "https://www.tripadvisor.ru/AttractionsNear-g298484-d4575960-Kursk_Station-Moscow_Central_Russia.html",
+            "https://www.tripadvisor.ru/AttractionsNear-g298484-d4575960-Kursk_Station-Moscow_Central_Russia.html"),
+
+"чкаловская": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d9575877-Kurskaya-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4575960-Kursk_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4575960-Kursk_Station-Moscow_Central_Russia.html"),
+
+"таганская": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d4577349-Taganka_Station-Moscow_Central_Russia.html",
+            "https://www.tripadvisor.ru/AttractionsNear-g298484-d15276786-Taganskaya_Metro_Station-Moscow_Central_Russia.html",
+            "https://www.tripadvisor.ru/AttractionsNear-g298484-d4577349-Taganka_Station-Moscow_Central_Russia.html"),
+
+"марксистская": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d4577349-Taganka_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d15276786-Taganskaya_Metro_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4577349-Taganka_Station-Moscow_Central_Russia.html"),
+
+"красные ворота": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d4575855-Red_Gates_Station-Moscow_Central_Russia.html",
+                    "https://www.tripadvisor.ru/AttractionsNear-g298484-d4575855-Red_Gates_Station-Moscow_Central_Russia.html",
+                    "https://www.tripadvisor.ru/AttractionsNear-g298484-d4575855-Red_Gates_Station-Moscow_Central_Russia.html"),
+
+"чистые пруды": ("https://www.tripadvisor.ru/Restaurants-g298484-zfn15621306-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/Attractions-g298484-Activities-zfn15621306-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/Attractions-g298484-Activities-c56-zfn15621306-Moscow_Central_Russia.html"),
+
+"тургеневская": ("https://www.tripadvisor.ru/Restaurants-g298484-zfn15621306-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/Attractions-g298484-Activities-zfn15621306-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/Attractions-g298484-Activities-c56-zfn15621306-Moscow_Central_Russia.html"),
+
+"сретенский бульвар": ("https://www.tripadvisor.ru/Restaurants-g298484-zfn15621306-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/Attractions-g298484-Activities-zfn15621306-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/Attractions-g298484-Activities-c56-zfn15621306-Moscow_Central_Russia.html"),
+
+"лубянка": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d4576080-Lubyanka_Station-Moscow_Central_Russia.html",
+            "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576080-Lubyanka_Station-Moscow_Central_Russia.html",
+            "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576080-Lubyanka_Station-Moscow_Central_Russia.html"),
+
+"кузнецкий мост": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d4576080-Lubyanka_Station-Moscow_Central_Russia.html",
+            "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576080-Lubyanka_Station-Moscow_Central_Russia.html",
+            "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576080-Lubyanka_Station-Moscow_Central_Russia.html"),
+
+"охотный ряд": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d301484-Okhotny_Ryad-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d301484-Okhotny_Ryad-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576586-Bird_Market_Station-Moscow_Central_Russia.html"),
+
+"театральная": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d301484-Okhotny_Ryad-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d301484-Okhotny_Ryad-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576586-Bird_Market_Station-Moscow_Central_Russia.html"),
+
+"площадь революции": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d301484-Okhotny_Ryad-Moscow_Central_Russia.html",
+                    "https://www.tripadvisor.ru/AttractionsNear-g298484-d301484-Okhotny_Ryad-Moscow_Central_Russia.html",
+                    "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576586-Bird_Market_Station-Moscow_Central_Russia.html"),
+
+"арбатская": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d9575849-Arbatskaya-Moscow_Central_Russia.html",
+            "https://www.tripadvisor.ru/Attractions-g298484-Activities-zfn15621305-Moscow_Central_Russia.html",
+            "https://www.tripadvisor.ru/Attractions-g298484-Activities-c56-zfn8707996-Moscow_Central_Russia.html"),
+
+"боровицкая": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d9575849-Arbatskaya-Moscow_Central_Russia.html",
+            "https://www.tripadvisor.ru/Attractions-g298484-Activities-zfn15621305-Moscow_Central_Russia.html",
+            "https://www.tripadvisor.ru/Attractions-g298484-Activities-c56-zfn8707996-Moscow_Central_Russia.html"),
+
+"александровский сад": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d9575849-Arbatskaya-Moscow_Central_Russia.html",
+                        "https://www.tripadvisor.ru/Attractions-g298484-Activities-zfn15621305-Moscow_Central_Russia.html",
+                        "https://www.tripadvisor.ru/Attractions-g298484-Activities-c56-zfn8707996-Moscow_Central_Russia.html"),
+
+"библиотека имени ленина": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d9575849-Arbatskaya-Moscow_Central_Russia.html",
+                            "https://www.tripadvisor.ru/Attractions-g298484-Activities-zfn15621305-Moscow_Central_Russia.html",
+                            "https://www.tripadvisor.ru/Attractions-g298484-Activities-c56-zfn8707996-Moscow_Central_Russia.html"),
+
+"кропоткинская": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d4575878-Kropotkinskaya_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4575878-Kropotkinskaya_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4575878-Kropotkinskaya_Station-Moscow_Central_Russia.html"),
+
+"маяковская": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d14200972-Mayakovskaya_Metro_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576179-Mayakovsky_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576179-Mayakovsky_Station-Moscow_Central_Russia.html"),
+
+"новокузнецкая": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d4576428-Novokuznetsk_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576428-Novokuznetsk_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576428-Novokuznetsk_Station-Moscow_Central_Russia.html"),
+
+"третьяковская": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d4576428-Novokuznetsk_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576428-Novokuznetsk_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576428-Novokuznetsk_Station-Moscow_Central_Russia.html"),
+
+"новослободская": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d4576442-Novoslobodskaya_Station-Moscow_Central_Russia.html",
+                    "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576442-Novoslobodskaya_Station-Moscow_Central_Russia.html",
+                    "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576442-Novoslobodskaya_Station-Moscow_Central_Russia.html"),
+
+"менделеевская": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d4576442-Novoslobodskaya_Station-Moscow_Central_Russia.html",
+                    "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576442-Novoslobodskaya_Station-Moscow_Central_Russia.html",
+                    "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576442-Novoslobodskaya_Station-Moscow_Central_Russia.html"),
+
+"проспект мира": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d15276772-oa30-Prospekt_Mira_Metro_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576854-Peace_Avenue_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576854-Peace_Avenue_Station-Moscow_Central_Russia.html"),
+
+"сухаревская": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d15276772-oa30-Prospekt_Mira_Metro_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576854-Peace_Avenue_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576854-Peace_Avenue_Station-Moscow_Central_Russia.html"),
+
+"цветной бульвар": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d4577692-Flower_Boulevard_Station-Moscow_Central_Russia.html",
+                    "https://www.tripadvisor.ru/AttractionsNear-g298484-d4577692-Flower_Boulevard_Station-Moscow_Central_Russia.html",
+                    "https://www.tripadvisor.ru/AttractionsNear-g298484-d4577692-Flower_Boulevard_Station-Moscow_Central_Russia.html"),
+
+"трубная": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d4577692-Flower_Boulevard_Station-Moscow_Central_Russia.html",
+                    "https://www.tripadvisor.ru/AttractionsNear-g298484-d4577692-Flower_Boulevard_Station-Moscow_Central_Russia.html",
+                    "https://www.tripadvisor.ru/AttractionsNear-g298484-d4577692-Flower_Boulevard_Station-Moscow_Central_Russia.html"),
+
+"баррикадная": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d4574639-Barricade_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4574639-Barricade_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4574639-Barricade_Station-Moscow_Central_Russia.html"),
+
+"краснопресненская": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d4574639-Barricade_Station-Moscow_Central_Russia.html",
+                    "https://www.tripadvisor.ru/AttractionsNear-g298484-d4574639-Barricade_Station-Moscow_Central_Russia.html",
+                    "https://www.tripadvisor.ru/AttractionsNear-g298484-d4574639-Barricade_Station-Moscow_Central_Russia.html"),
+
+"серпуховская": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d4577133-Serpukhovskaya_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4577133-Serpukhovskaya_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4577133-Serpukhovskaya_Station-Moscow_Central_Russia.html"),
+
+"добрынинская": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d4577133-Serpukhovskaya_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4577133-Serpukhovskaya_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4577133-Serpukhovskaya_Station-Moscow_Central_Russia.html"),
+
+"полянка": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d4576769-Polyanka_Station-Moscow_Central_Russia.html",
+            "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576769-Polyanka_Station-Moscow_Central_Russia.html",
+            "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576769-Polyanka_Station-Moscow_Central_Russia.html"),
+
+"смоленская": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d9575848-oa30-Smolenskaya-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/Attraction_Review-g298484-d15276777-Reviews-Smolenskaya_Metro_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/Attraction_Review-g298484-d15276777-Reviews-Smolenskaya_Metro_Station-Moscow_Central_Russia.html"),
+
+"киевская": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d15266252-Kiyevskaya_Metro_Station-Moscow_Central_Russia.html",
+            "https://www.tripadvisor.ru/AttractionsNear-g298484-d4575644-Kiev_Station-Moscow_Central_Russia.html",
+            "https://www.tripadvisor.ru/AttractionsNear-g298484-d4575644-Kiev_Station-Moscow_Central_Russia.html"),
+
+"парк культуры": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d4576605-Culture_Park_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576605-Culture_Park_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4576605-Culture_Park_Station-Moscow_Central_Russia.html"),
+
+"павелецкая": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d7914834-Paveletskiy_Train_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d7914834-Paveletskiy_Train_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d7914834-Paveletskiy_Train_Station-Moscow_Central_Russia.html"),
+
+"белорусская": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d4574704-Belorussia_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4574704-Belorussia_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/AttractionsNear-g298484-d4574704-Belorussia_Station-Moscow_Central_Russia.html"),
+
+"октябрьская": ("https://www.tripadvisor.ru/RestaurantsNear-g298484-d4576528-October_Station-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/Attraction_Review-g298484-d15324909-Reviews-Metro_Station_Oktyabrskaya-Moscow_Central_Russia.html",
+                "https://www.tripadvisor.ru/Attraction_Review-g298484-d15324909-Reviews-Metro_Station_Oktyabrskaya-Moscow_Central_Russia.html")
+}
