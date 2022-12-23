68c083c4689501d6bf162936453a33e7b0ac6701
verapasichenko <115879263+verapasichenko@users.noreply.github.com>
2022-12-23 17:41:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import random
 import telebot
 from pyowm import OWM
 from telebot import types
@@ -5,7 +6,6 @@ from Token import BotToken
 
 from lib.database import Database
 from lib.stations import stations_data
-
 
 db = Database()
 bot = telebot.TeleBot(BotToken)
@@ -27,10 +27,12 @@ def weather(city: str):
     :param city: вводим город
     :return: выводим температуру и локацию
     '''
+
+
     owm = OWM('4b4ba7fd4d10e6a206a383cc2bba05dc')
-    mgr = owm.weather_manager()
-    observation = mgr.weather_at_place(city)
-    weather = observation.weather
+    mgr = owm.weather_manager()#возвращаем объект для получения погоды
+    observation = mgr.weather_at_place(city)#назначаем где нам нужно получить погоду
+    weather = observation.weather#получаем саму погоду, используя свойство
     location = get_location(observation.location.lat, observation.location.lon)
     temperature = weather.temperature("celsius")
     return temperature, location
@@ -79,9 +81,11 @@ def get_start_message(message):
     btn1 = types.KeyboardButton('/help')
     btn2 = types.KeyboardButton('/weather')
     btn3 = types.KeyboardButton('/add')
-    markup.row(btn1, btn2, btn3)
+    btn4 = types.KeyboardButton('/goroskop')
+
+    markup.row(btn1, btn2, btn3, btn4)
     bot.send_message(message.from_user.id,
-                        "Привет, {0.first_name}! Я, Даша-путешественница, помогу тебе провести незабываемое время в Москве! По команде: \n/help можно станцию метро\n/weather можно узнать о погоде в Москве\n/add ты можешь добавить места, которых нет в списке, чтобы поделиться своими впечатлениями, а мы их добавим в список".format(
+                        "Привет, {0.first_name}! Я, Даша-путешественница, помогу тебе провести незабываемое время в Москве! По команде: \n/help можно станцию метро\n/weather можно узнать о погоде в Москве\n/add ты можешь добавить места, которых нет в списке, чтобы поделиться своими впечатлениями, а мы их добавим в список\n/goroskop ты можешь узнать свой гороскоп на день".format(
                             message.from_user), reply_markup=markup)
 
 
@@ -117,14 +121,14 @@ def get_station(message):
                             reply_markup=keyboard)
 
 
-@bot.callback_query_handler(func=lambda call: True)
+@bot.callback_query_handler(func=lambda call: len(call.data.split(',')) == 2)#принимает анонимную функциюи возвращает True
 def callback_worker(call):
     '''
     Функция принимает выбранное место и выдает вариант ссылок на развлечения
     :param call: выбранное место
     :return: ссылку на развлечение
     '''
-    station, type = call.data.split(',')
+    station, type = call.data.split(',')#возвращаем список станций и ссылок через запятую
     bot.send_message(call.message.chat.id, text="Отличный выбор!")
     bot.send_message(call.message.chat.id, text=stations_data[station][int(type)])
 
@@ -147,12 +151,78 @@ def add_review(message):
     :return: заносит сообщение в таблицу
     '''
     db.add_review(message)
+    bot.send_message(message.chat.id, 'Спасибо, отзыв добавлен! Хочешь прочитать отзывы других людей? [Да/Нет]:')
+    bot.register_next_step_handler(message, read_reviews)
 
+def read_reviews(message):
+    if message.text.lower() == 'да':
+        reviews = "\n".join(db.get_reviews())
+        bot.send_message(message.chat.id, reviews)
+    else:
+        bot.send_message(message.chat.id, 'Хорошо, пока!')
+
+
+first = ["Сегодня — идеальный день для новых начинаний.","Оптимальный день для того, чтобы решиться на смелый поступок!","Будьте осторожны, сегодня звёзды могут повлиять на ваше финансовое состояние.","Лучшее время для того, чтобы начать новые отношения или разобраться со старыми.","Плодотворный день для того, чтобы разобраться с накопившимися делами."]
+second = ["Но помните, что даже в этом случае нужно не забывать про","Если поедете за город, заранее подумайте про","Те, кто сегодня нацелен выполнить множество дел, должны помнить про","Если у вас упадок сил, обратите внимание на","Помните, что мысли материальны, а значит вам в течение дня нужно постоянно думать про"]
+second_add = ["отношения с друзьями и близкими.","работу и деловые вопросы, которые могут так некстати помешать планам.","себя и своё здоровье, иначе к вечеру возможен полный раздрай.","бытовые вопросы — особенно те, которые вы не доделали вчера.","отдых, чтобы не превратить себя в загнанную лошадь в конце месяца."]
+third = ["Злые языки могут говорить вам обратное, но сегодня их слушать не нужно.","Знайте, что успех благоволит только настойчивым, поэтому посвятите этот день воспитанию духа.","Даже если вы не сможете уменьшить влияние ретроградного Меркурия, то хотя бы доведите дела до конца.","Не нужно бояться одиноких встреч — сегодня то самое время, когда они значат многое.","Если встретите незнакомца на пути — проявите участие, и тогда эта встреча посулит вам приятные хлопоты."]
+fourth = ["Мы сдали аип", "Мы не идем на пересдачу"]
+# Метод, который получает сообщения и обрабатывает их
+@bot.message_handler(commands=['goroskop'])
+def get_gor_message(message):
+    # Если написали «Привет»
+    if message.text == "/goroskop":
+        # Пишем приветствие
+        bot.send_message(message.from_user.id, "Привет, сейчас я расскажу тебе гороскоп на сегодня.")
+        # Готовим кнопки
+        keyboard = types.InlineKeyboardMarkup()
+        # По очереди готовим текст и обработчик для каждого знака зодиака
+        key_oven = types.InlineKeyboardButton(text='Овен', callback_data='zodiac')
+        # И добавляем кнопку на экран
+        keyboard.add(key_oven)
+        key_telec = types.InlineKeyboardButton(text='Телец', callback_data='zodiac')
+        keyboard.add(key_telec)
+        key_bliznecy = types.InlineKeyboardButton(text='Близнецы', callback_data='zodiac')
+        keyboard.add(key_bliznecy)
+        key_rak = types.InlineKeyboardButton(text='Рак', callback_data='zodiac')
+        keyboard.add(key_rak)
+        key_lev = types.InlineKeyboardButton(text='Лев', callback_data='zodiac')
+        keyboard.add(key_lev)
+        key_deva = types.InlineKeyboardButton(text='Дева', callback_data='zodiac')
+        keyboard.add(key_deva)
+        key_vesy = types.InlineKeyboardButton(text='Весы', callback_data='zodiac')
+        keyboard.add(key_vesy)
+        key_scorpion = types.InlineKeyboardButton(text='Скорпион', callback_data='zodiac')
+        keyboard.add(key_scorpion)
+        key_strelec = types.InlineKeyboardButton(text='Стрелец', callback_data='zodiac')
+        keyboard.add(key_strelec)
+        key_kozerog = types.InlineKeyboardButton(text='Козерог', callback_data='zodiac')
+        keyboard.add(key_kozerog)
+        key_vodoley = types.InlineKeyboardButton(text='Водолей', callback_data='zodiac')
+        keyboard.add(key_vodoley)
+        key_ryby = types.InlineKeyboardButton(text='Рыбы', callback_data='zodiac')
+        keyboard.add(key_ryby)
+        # Показываем все кнопки сразу и пишем сообщение о выборе
+        bot.send_message(message.from_user.id, text='Выбери свой знак зодиака', reply_markup=keyboard)
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши Привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+# Обработчик нажатий на кнопки
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker1(call):
+    # Если нажали на одну из 12 кнопок — выводим гороскоп
+    if call.data == "zodiac":
+        # Формируем гороскоп
+        msg1 = random.choice(first) + ' ' + random.choice(second) + ' ' + random.choice(second_add) + ' ' + random.choice(third) + ' ' + random.choice(fourth)
+        # Отправляем текст в Телеграм
+        bot.send_message(call.message.chat.id, msg1)
 
 @bot.message_handler(content_types=['text'])
 def handle_bad_message(message):
     ''' Ответ на непонятные сообщения '''
-    if message.text not in ('/start', '/add', '/help', '/weather'):
+    if message.text not in ('/start', '/add', '/help', '/weather', '/gor'):
         bot.send_message(message.chat.id, text='Я тебя не понимаю. Напиши /start.')
 
 bot.polling(none_stop=True, interval=0)
+
